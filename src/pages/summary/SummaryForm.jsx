8d010acda1b1e615b4d6caff6e707d7f782a1ad6
vvictorious <vvictorious30@gmail.com>
2021-06-16 13:05:23 -0600

    tests are passing with the addition of the new component

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,3 +1,29 @@
+import React, { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 export default function SummaryForm() {
-    return <div />
+	const [tcChecked, setTcChecked] = useState(false)
+
+	const checkBoxLabel = (
+		<span>
+			I agree to <span style={{ color: 'blue' }}> Terms and conditions</span>
+		</span>
+	);
+
+	return (
+		<Form>
+			<Form.Group controlId="terms-and-conditions">
+				<Form.Check
+					type='checkbox'
+					checked={tcChecked}
+					onChange={(e) => setTcChecked(e.target.checked)}
+					label={checkBoxLabel}
+				/>
+			</Form.Group>
+			<Button variant='primary' type='submit' disabled={!tcChecked}>
+				Confirm order
+			</Button>
+		</Form>
+	)
 }
